8c89684ae55a5834d8a70a680c4bcb2d42e73db1
obakaito05 <1511530029j@gmail.com>
2023-04-09 16:54:48 +0900

    ユーザー管理機能の修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
 
          validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
          validates :nickname, presence: true
-         validates :surname, presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥]+\z/ }
-         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥]+\z/ }
+         validates :surname, presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/ }
+         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/ }
          validates :surname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
          validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
          validates :birthday, presence: true
