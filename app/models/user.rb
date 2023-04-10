3de8ba4b56cfb84a023da91d166e80dba785d3d5
obakaito05 <1511530029j@gmail.com>
2023-04-10 16:49:51 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
         # has_many :comments
 
-         validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
          validates :nickname, presence: true
          validates :surname, presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/ }
          validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/ }
