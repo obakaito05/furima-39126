d2263d31e520edf29061f3647b91ee1cc81d1703
obakaito05 <1511530029j@gmail.com>
2023-04-10 17:14:51 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
 
          validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
          validates :nickname, presence: true
-         validates :surname, presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/ }
-         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/ }
+         validates :surname, presence: true, format: { with: /\A[ぁ-んァ-ヶー一龥々ー]+\z/ }
+         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー一龥々ー]+\z/ }
          validates :surname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
          validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
          validates :birthday, presence: true
